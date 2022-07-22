ac7d087df47c44e83a063f5fc75a980e3a6a21b9
juno-choi <ililil9482@naver.com>
2022-07-22 17:10:57 +0900

    reflection을 사용하여 singleton 깨기

--- src/main/java/com/example/design/singleton/App.java
+++ src/main/java/com/example/design/singleton/App.java
@@ -1,9 +1,15 @@
 package com.example.design.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         Settings settings1 = Settings.getInstance();
-        Settings settings2 = Settings.getInstance();
+        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings settings2 = constructor.newInstance();
+
         System.out.println(settings1 == settings2);
     }
 }
