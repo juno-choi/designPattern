927215c1499c00e2d1caeaf68362bb9b66a93c1d
juno-choi <ililil9482@naver.com>
2022-07-22 17:33:01 +0900

    enum을 사용하여 싱글톤 구현

--- src/main/java/com/example/design/singleton/App.java
+++ src/main/java/com/example/design/singleton/App.java
@@ -1,19 +1,9 @@
 package com.example.design.singleton;
 
-import java.io.*;
-
 public class App {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Settings settings1 = Settings.getInstance();
-        Settings settings2 = null;
-
-        try(ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))){
-            out.writeObject(settings1);
-        }
-
-        try(ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))){
-            settings2 = (Settings) in.readObject();
-        }
+    public static void main(String[] args) {
+        Settings settings1 = Settings.INSTANCE;
+        Settings settings2 = Settings.INSTANCE;
 
         System.out.println(settings1 == settings2);
     }
--- src/main/java/com/example/design/singleton/Settings.java
+++ src/main/java/com/example/design/singleton/Settings.java
@@ -1,20 +1,5 @@
 package com.example.design.singleton;
 
-import java.io.Serializable;
-
-public class Settings implements Serializable {
-
-    private Settings(){}
-
-    private static class SettingHolder{
-        private static final Settings INSTANCE = new Settings();
-    }
-
-    public static synchronized Settings getInstance(){
-        return SettingHolder.INSTANCE;
-    }
-
-    protected Object readResolve(){
-        return getInstance();
-    }
+public enum Settings {
+    INSTANCE;
 }
